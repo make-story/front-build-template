af27e62c207e98cb1483f7095b376c2afd2806b6
Sung-min Yu <yu9221@gmail.com>
2021-01-11 20:23:35 +0900

    sh

--- shell/build.sh
+++ shell/build.sh
@@ -6,29 +6,30 @@
 # $ sh shell/server.sh 'master' '빌드번호'
 
 #SHELL_PATH=`pwd -P`
-SHELL_PATH="/Users/sung-minyu/Development/github"
+PROJECT_PATH="/usr/src/project"
+PROJECT_NAME="makestory"
 if [ -n "$1" ] && [ -n "$2" ];
-    then
+	then
 
-    # "env"
-    export BUILD_NUMBER=$2 # '/빌드번호/빌드결과' 형태로 경로 생성
-    if [ "develop" == "$1" ] || [ "origin/develop" == "$1" ];
-        then
-        SHELL_PATH=$SHELL_PATH/webpagetest.test
-        source $SHELL_PATH/env/test.env; 
-    elif [ "master" == "$1" ] || [ "origin/master" == "$1" ];
-        then
-        SHELL_PATH=$SHELL_PATH/webpagetest
-        source $SHELL_PATH/env/production.env;
-    else 
-        echo "Git 브랜치 조건확인이 필요합니다."
-        exit;
-    fi
+	# "env"
+	export BUILD_NUMBER=$2 # '/빌드번호/빌드결과' 형태로 경로 생성
+	if [ "develop" == "$1" ] || [ "origin/develop" == "$1" ];
+		then
+		PROJECT_PATH=$PROJECT_PATH/$PROJECT_NAME.test
+		source $PROJECT_PATH/env/test.env; 
+	elif [ "master" == "$1" ] || [ "origin/master" == "$1" ];
+		then
+		PROJECT_PATH=$PROJECT_PATH/$PROJECT_NAME
+		source $PROJECT_PATH/env/production.env;
+	else 
+		echo "Git 브랜치 조건확인이 필요합니다."
+		exit;
+	fi
 
-    # "build"
-    $SHELL_PATH/node_modules/.bin/webpack --config $SHELL_PATH/config/webpack.config.js --mode production
-    unset BUILD_NUMBER
+	# "build"
+	$PROJECT_PATH/node_modules/.bin/webpack --config $PROJECT_PATH/webpack.config.js --mode production
+	unset BUILD_NUMBER
 else 
-    echo "설정값 확인이 필요합니다."
+	echo "설정값 확인이 필요합니다."
 fi
 exit;
