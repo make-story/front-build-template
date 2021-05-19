12efe26c9e45b1a886f8a70fb8d4774777725e8d
Sung-min Yu <yu9221@gmail.com>
2021-05-19 13:05:44 +0900

    update

---
+++ servers/nodeRequireTest/node.js
@@ -0,0 +1,6 @@
+const module1 = require('./module1');
+const module2 = require('./module2');
+
+/*
+결국 test.js 는 한번만 실행된다!
+*/
---
+++ servers/nodeRequireTest/test.js
@@ -0,0 +1,6 @@
+const test = 'test';
+console.log(test);
+
+module.exports = {
+    test
+};
