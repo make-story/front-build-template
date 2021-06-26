9428b00d3b53e2d6b2ee229ded012ca6ab715e23
Sung-min Yu <yu9221@gmail.com>
2021-06-26 23:56:04 +0900

    update

---
+++ test/sync.js
@@ -0,0 +1,16 @@
+// 동기-블로킹 방식
+const fs = require('fs');
+
+console.log('시작');
+let data = null;
+
+data = fs.readFileSync('./readme2.txt');
+console.log('1번', data.toString());
+
+data = fs.readFileSync('./readme2.txt');
+console.log('2번', data.toString());
+
+data = fs.readFileSync('./readme2.txt');
+console.log('3번', data.toString());
+
+console.log('끝');
